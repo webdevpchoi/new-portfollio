6a3a2783fd482814c9f38b41c1c494539bef6d95
WonkasWilly <pchoi13370@gmail.com>
2018-10-24 13:26:14 -0700

    set up the base design of the portfolio and included a viewport helper function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
-console.log('JS file connected.');
+const testBtn = document.querySelector('.test-btn');
+const curtainContainer = document.querySelector('.curtain-container');
+const curtain = document.querySelector('.curtain');
+const portfolio = document.querySelector('.portfolio');
+//test button
+testBtn.addEventListener('click', function() {
+	alert('this works!');
+})
+
+//helper function
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
