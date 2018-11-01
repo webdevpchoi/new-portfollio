ee4b5cb454279d987d791e241ab81107f63e610e
WonkasWilly <pchoi13370@gmail.com>
2018-11-01 10:12:03 -0700

    added animation event listener for whiteout animation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,16 @@
 window.addEventListener('DOMContentLoaded', function(event) {
 	'use strict';
 
-	const lightBulb = document.querySelector('.lightbulb-img');
+	const lightBulb= document.querySelector('.lightbulb');
+	const lightBulbImg = document.querySelector('.lightbulb-img');
 	const whiteOut = document.querySelector('.whiteout');
 
-	lightBulb.addEventListener('click', function() {
+	lightBulbImg.addEventListener('click', function() {
 		this.setAttribute('src', './assets/icons/lightbulb-yellow.svg');
 		whiteOut.classList.add('animate-whiteout');
 	})
-	
+
+	whiteOut.addEventListener('animationend', function() {
+		lightBulb.classList.add('transition-lightbulb');
+	});
 })
