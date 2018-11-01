c7bf8b93deb9ea791d64a8ad0f23a4ac007709de
Phillip Choi <pchoi13370@gmail.com>
2018-10-31 23:21:52 -0700

    Added transition into portfolio webpage from cover page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,12 @@
 window.addEventListener('DOMContentLoaded', function(event) {
 	'use strict';
+
+	const lightBulb = document.querySelector('.lightbulb-img');
+	const whiteOut = document.querySelector('.whiteout');
+
+	lightBulb.addEventListener('click', function() {
+		this.setAttribute('src', './assets/icons/lightbulb-yellow.svg');
+		whiteOut.classList.add('animate-whiteout');
+	})
 	
 })
