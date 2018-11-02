094f27dd83dbdd1f0767c796de46f07c08358049
Phillip Choi <pchoi13370@gmail.com>
2018-11-02 00:14:53 -0700

    working on restructing layout for smoother animation transitions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,13 @@
 window.addEventListener('DOMContentLoaded', function(event) {
 	'use strict';
-
-	const lightBulb= document.querySelector('.lightbulb');
-	const lightBulbImg = document.querySelector('.lightbulb-img');
+	
+	const lightBulb = document.querySelector('.lightbulb-img');
 	const whiteOut = document.querySelector('.whiteout');
 
-	lightBulbImg.addEventListener('click', function() {
+	
+	lightBulb.addEventListener('click', function() {
 		this.setAttribute('src', './assets/icons/lightbulb-yellow.svg');
 		whiteOut.classList.add('animate-whiteout');
 	})
-
-	whiteOut.addEventListener('animationend', function() {
-		lightBulb.classList.add('transition-lightbulb');
-	});
+	
 })
