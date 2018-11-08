b8df04df1dd40004709de839104fe96893e10309
Phillip Choi <pchoi13370@gmail.com>
2018-11-07 23:37:44 -0800

    added in some test content to work on animating the layout, as well as added in helper js function to detect if elements are in the viewport

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,22 @@
 window.addEventListener('DOMContentLoaded', function(event) {
 	'use strict';
-	
-	const lightBulb = document.querySelector('.lightbulb-img');
-	const whiteOut = document.querySelector('.whiteout');
 
-	
-	lightBulb.addEventListener('click', function() {
-		this.setAttribute('src', './assets/icons/lightbulb-yellow.svg');
-		whiteOut.classList.add('animate-whiteout');
+	let isInViewport = function(elem) {
+		const bounding = elem.getBoundingClientRect();
+		return (
+			bounding.top >= 0 &&
+			bounding.left >= 0 &&
+			bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+			bounding.left <= (window.innerHeight || document.documentElement.clientHeight)
+		)
+	}
+
+	const container = document.querySelector('.container');
+	container.addEventListener('scroll', function() {
+		if(isInViewport(?????)) {
+			//is in the viewport
+		} else {
+			//not in the viewport
+		}
 	})
-	
 })
